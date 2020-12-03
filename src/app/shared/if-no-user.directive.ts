2927fae6cd5782a0b19ab9f41a550a6e77c663ef
Ali Labiadh <ali.labiadh@aliznet.fr>
2020-12-03 16:38:03 +0100

    '3-Acceuil-Refacto'

--- src/app/shared/if-no-user.directive.ts
+++ src/app/shared/if-no-user.directive.ts
@@ -1,12 +1,14 @@
-import {Directive, HostBinding, OnInit} from '@angular/core';
+import {Directive, HostBinding, OnDestroy, OnInit} from '@angular/core';
 import {User} from '../user/models/user';
 import {UserService} from '../user/user.service';
+import {Subscription} from 'rxjs';
 
 @Directive({
   selector: '[appIfNoUser]'
 })
-export class IfNoUserDirective implements OnInit{
+export class IfNoUserDirective implements OnInit, OnDestroy{
 
+  userDetectSub: Subscription = new Subscription();
   @HostBinding('class') dNone = '';
   user: User | null = null;
   constructor(private userService: UserService) {
@@ -22,6 +24,10 @@ export class IfNoUserDirective implements OnInit{
   ngOnInit(): void {
     this.user = this.userService.user;
     this.update(this.user);
+  }
+
+  ngOnDestroy(): void {
+    this.userDetectSub.unsubscribe();
   }
 
 }
